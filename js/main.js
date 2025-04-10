298b7a93aaabfe1156e572a2c53739b9e8cb0037
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-04-10 04:16:17 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var britshFlag = document.querySelector('.britsh-flag')
 deutschFlag.addEventListener('click' , function () {
     britshFlag.classList.replace('d-none', 'd-flex')
     deutschFlag.classList.add('d-none')
-    console.log('111111111111');
     
 })
 
@@ -12,3 +11,27 @@ britshFlag.addEventListener('click' , function () {
     deutschFlag.classList.replace('d-none','d-flex')
     britshFlag.classList.replace('d-flex','d-none')
 })
+
+
+let offsetTop = $('#about').offset().top
+$(window).scroll(function () {
+    let scrollWindow = $(window).scrollTop()
+    if (scrollWindow > offsetTop-110) {
+        $('.navbar').css('backgroundColor','rgba(0,0,0,0.9)')
+    }else{
+        $('.navbar').css('backgroundColor','transparent')
+        
+    }
+    
+})
+
+$('a[href^="#"]').click(function (info) {
+    let hRef = info.target.getAttribute('href') 
+    let offsetTopNav = $(hRef).offset().top
+    $('body').animate({scrollTop:offsetTopNav} , 2000)
+    console.log(hRef,offsetTopNav);
+    
+});
+
+
+
