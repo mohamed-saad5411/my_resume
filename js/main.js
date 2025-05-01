7d9f8a56a33ef9658228fdc28740d1053b07fa75
mohamed saad <124407370+mohamed-saad5411@users.noreply.github.com>
2025-05-01 03:12:27 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -33,5 +33,23 @@ $('a[href^="#"]').click(function (info) {
     
 });
 
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2000,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 200*10^1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+ Source
 
 
